2f80fe5c2bcbcb34ff2abe671b40a9b310bb3a98
Pavel Šmejkal <smejkal.pa@gmail.com>
2020-09-01 20:01:44 +0200

    Add alpha blending

--- game/src/render/Render.cpp
+++ game/src/render/Render.cpp
@@ -1005,19 +1005,17 @@ RESULT Render::PrepareForDraw()
         depthStencil.depthCompareOp     = VK_COMPARE_OP_LESS;
     }
 
-    VkPipelineColorBlendAttachmentState colorBlendAttachment{};
-    //colorBlendAttachment.blendEnable = VK_TRUE;
-    //colorBlendAttachment.srcColorBlendFactor;
-    //VkBlendFactor            dstColorBlendFactor;
-    //VkBlendOp                colorBlendOp;
-    //VkBlendFactor            srcAlphaBlendFactor;
-    //VkBlendFactor            dstAlphaBlendFactor;
-    //VkBlendOp                alphaBlendOp;
-    //VkColorComponentFlags    colorWriteMask;
-
     VkPipelineColorBlendStateCreateInfo colorBlending{};
     {
-        colorBlendAttachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
+        VkPipelineColorBlendAttachmentState colorBlendAttachment{};
+        colorBlendAttachment.blendEnable            = VK_TRUE;
+        colorBlendAttachment.srcColorBlendFactor    = VK_BLEND_FACTOR_SRC_ALPHA;
+        colorBlendAttachment.dstColorBlendFactor    = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA;
+        colorBlendAttachment.colorBlendOp           = VK_BLEND_OP_ADD;
+        colorBlendAttachment.srcAlphaBlendFactor    = VK_BLEND_FACTOR_ONE;
+        colorBlendAttachment.dstAlphaBlendFactor    = VK_BLEND_FACTOR_ONE;
+        colorBlendAttachment.alphaBlendOp           = VK_BLEND_OP_MAX;
+        colorBlendAttachment.colorWriteMask         = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
 
         colorBlending.sType             = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
         colorBlending.attachmentCount   = 1;
