5cc11b1354375040828275311a3ef6f1f37f87ad
Pavel Šmejkal <smejkal.pa@gmail.com>
2021-03-01 21:08:02 +0100

    Add game name

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,8 +1,20 @@
 #include "EngineMain.h"
 
-//------------------------------------------------------------------------------
-int CALLBACK WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR cmdLine, int showCmd)
-{
-    int result = hs::EngineMainWin32(instance, prevInstance, cmdLine, showCmd);
-    return result;
-}
+constexpr const char* GAME_NAME = "Pixel Trader";
+
+#if HS_WINDOWS
+    //------------------------------------------------------------------------------
+    int CALLBACK WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR cmdLine, int showCmd)
+    {
+        int result = hs::EngineMainWin32(instance, prevInstance, cmdLine, GAME_NAME);
+        return result;
+    }
+#elif HS_LINUX
+    //------------------------------------------------------------------------------
+    int main(int argc, char** argv)
+    {
+        int result = hs::EngineMainLinux(argc, argv, GAME_NAME);
+        return result;
+    }
+#endif
+
